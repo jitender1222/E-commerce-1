53a05f5eb067dd789c0cc0a531c27a9e0d576a01
jitender singh <jeetusinghkanyal@gmail.com>
2023-01-06 22:32:44 +0530

    added a coupon schema

---
+++ backend/models/coupon.schema.js
@@ -0,0 +1,30 @@
+
+import mongoose from "mongoose";
+
+const couponSchema= mongoose.Schema({
+
+    code:{
+
+        type:String,
+        required:[true,"Please Provide a Coupon name"]
+    },
+
+    discount:{
+
+        type:Number,
+        default:0,
+    },
+
+    active:{
+
+        type:Boolean,
+        default:true,
+    }
+},
+
+{
+    timestamps:true
+}
+)
+
+export default mongoose.model('coupon',couponSchema);
