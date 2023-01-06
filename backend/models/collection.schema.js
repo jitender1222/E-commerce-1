d50d5bf9b4e6c58c1282a7b3d279896a07cec2cd
jitender singh <jeetusinghkanyal@gmail.com>
2023-01-06 22:32:02 +0530

    added a collection schema

--- backend/models/collection.schema.js
+++ backend/models/collection.schema.js
@@ -1,3 +1,22 @@
 
 
 import mongoose from "mongoose";
+
+
+const collectionSchema=new mongoose.Schema(
+    {
+        name:{
+            type:String,
+            required:[true,"please provide a category name"],
+            trim:true,
+            maxlength:[120,"Collection name should be less than or equal then 120 characters"],
+        },
+    },
+
+    {
+        timestamps:true
+    }
+)
+
+export default mongoose.model("Collections",collectionSchema);
+
