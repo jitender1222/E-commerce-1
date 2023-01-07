be08b9eeefbba6bd20947d96657f47e43a49d205
jitender singh <jeetusinghkanyal@gmail.com>
2023-01-07 16:50:01 +0530

    add port

--- backend/config/index.js
+++ backend/config/index.js
@@ -6,6 +6,8 @@ const config={
 
     JWT_SECRET: process.env.JWT_SECRET,
     JWT_EXPIRY: process.env.JWT_EXPIRY || "30d",
+    MONGODB_URL:process.env.MONGODB_URL,
+    PORT:process.env.PORT
 }
 
 export default config
