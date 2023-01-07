7437b006cdf102693de0f006e72b00b4f55be436
jitender singh <jeetusinghkanyal@gmail.com>
2023-01-07 23:29:14 +0530

    added login and logout

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -39,7 +39,7 @@ export const signUp=asyncHandler(async(req,res) =>{
 
     if(existingUser){
 
-        throw new CustomError('Please fill all the fields',400);
+        throw new CustomError('user is already exist',400);
     }
 
     const user=await User.create({
@@ -52,4 +52,88 @@ export const signUp=asyncHandler(async(req,res) =>{
     const token=user.getJwtToken();
     console.log(user);
     user.password=undefined;
+
+    res.cookie("token",token,cookieOptions);
+
+    res.status(200).json({
+        success:true,
+        token,
+        user
+    })
+})
+
+
+/*********************************************************
+ * @LOGIN
+ * @route http://localhost:5000/api/auth/login
+ * @description User login Controller for logging new user
+ * @Parameters email,password
+ * @returns User Object
+
+*********************************************************/
+
+export const login=asyncHandler(async(req,res) =>{
+
+    const {email,password}=req.body
+
+    if(!email || !password){
+
+        throw new CustomError('Please fill all the fields',400);
+    }
+
+    // check if the user exist
+
+    const user= await User.findOne({email}).select("+password");
+
+    if(!user){
+
+        throw new CustomError('Invalid Credentails',400);
+    }
+
+    const isPasswordMatched=await user.comparePassword(password);
+
+    if(!isPasswordMatched){
+
+        throw new CustomError('Invalid Credentials -pass',400)
+    }
+
+    const token=user.getJwtToken()
+    user.password=undefined;
+    res.cookie("token",token,cookieOptions)
+
+    return res.status(200).json({
+
+        success:true,
+        token,
+        user,
+    })
+})
+
+
+
+
+/*********************************************************
+ * @LOGOUT
+ * @route http://localhost:5000/api/auth/logout
+ * @description User 
+ * @Parameters 
+ * @returns 
+
+*********************************************************/
+
+export const logout=asyncHandler(async(req,res)=>{
+
+    // we can also use cookie.clear
+
+    res.cookie("token",null,{
+
+        expires:new Date(Date.now()),
+        httpOnly:true,
+    })
+
+    res.status(200).json({
+
+        success:true,
+        message:"Logged Out"
+    })
 })
